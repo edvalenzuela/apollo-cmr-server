1c2b289457183bff5c65a22f15ad5d118ec0d82d
edvalenzuela <edvalenzuelap@gmail.com>
2024-05-19 01:49:22 -0400

    feat: add populate and add information for cliente

--- db/schema.js
+++ db/schema.js
@@ -36,7 +36,7 @@ export const typeDefs = gql`
     id: ID
     pedido: [PedidoGrupo]
     total: Float
-    cliente: ID
+    cliente: Cliente
     vendedor: ID
     fecha: String
     estado: EstadoPedido
